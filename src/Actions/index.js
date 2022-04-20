556f6d3cce0eda641486c0131dca46bb867b9594
Carlos Racca <carlosracca1@gmail.com>
2022-04-20 11:46:38 -0300

    llamadas a las rutas del back

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,1 +1,52 @@
 import axios from 'axios';
+
+
+export function getScores(){
+
+    return async function(dispatch){
+        
+        let allScores = await axios.get('https://sdga-tour.herokuapp.com/scores')
+
+        let scores = allScores.data.map(el => el)
+
+        return dispatch({
+            type: 'GET_SCORES',
+            payload: scores
+        })
+    }
+}
+
+export function getUsers(){
+
+    return async function(dispatch){
+        
+        let allUsers = await axios.get('https://sdga-tour.herokuapp.com/users')
+
+        let users = allUsers.data.map(el => el)
+
+        return dispatch({
+            type: 'GET_USERS',
+            payload: users
+        })
+    }
+}
+
+export function postScore(payload){
+
+    return async function(dispatch){
+
+        const response = await axios.post('https://sdga-tour.herokuapp.com/score', payload)
+
+        return response
+    }
+}
+
+export function postUser(payload){
+
+    return async function(dispatch){
+
+        const response = await axios.post('https://sdga-tour.herokuapp.com/user', payload)
+
+        return response
+    }
+}
--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -1,13 +1,37 @@
 
 const initialState = {
-    scores: []
+    scores: [],
+    users: []
 };
 
 function rootReducer(state = initialState, action){
     switch(action.type){
         default:
             return state;
+
+        case 'GET_USERS':
+            return {
+                ...state,
+                users: action.payload
+            };
+
+        case 'GET_SCORES':
+            return {
+                ...state,
+                scores: action.payload
+            };
+
+        case 'POST_USER':
+            return {
+                ...state,
+            };
+
+        case 'POST_SCORE':
+            return {
+                ...state,
+            };
     };
+    
 };
 
 export default rootReducer;
