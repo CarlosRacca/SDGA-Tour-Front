1ce1d8a108022f6dfd85c10df31eb2f0eb05d0a9
Carlos Racca <carlosracca1@gmail.com>
2022-04-26 20:20:00 -0300

    categorias y ordenamiento

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -24,6 +24,30 @@ export default function Home(){
     const dates = useSelector(state => state.dates);
     const scoresUser = useSelector(state => state.scoresUser);
 
+    function orderByScoresTotal(){
+        scoresUser.forEach(el => {
+            if(el.scores.length >= 3){
+                el.sum = []
+                el.scores.sort((a, b) => a.totalNeto - b.totalNeto)
+                el.scores.forEach(score => {
+                    if(el.scores.indexOf(score) === 0){
+                        el.sum.push(score.totalNeto)
+                    }
+                    if(el.scores.indexOf(score) === 1){
+                        el.sum.push(score.totalNeto)
+                    }
+                    if(el.scores.indexOf(score) === el.scores.length - 1){
+                        el.sum.push(score.totalNeto)
+                    }
+                })
+
+                el.total = [el.sum[0] + el.sum[1] + el.sum[2]]
+            }
+        })
+
+        scoresUser.sort((a,b) => a.total - b.total)
+    }
+
     function numberToMonth(month){
         if(month === '01'){
             return "ENERO"
@@ -100,8 +124,12 @@ export default function Home(){
                             <th>A</th>
                         </tr>
                         {
+                            orderByScoresTotal()
+                        }
+                        {   
                             scoresUser ? scoresUser.map(el => {
                                 var sum = []
+                                if(el.scores[0].categoria === "A"){
                                 return(
                                 <tr>
                                     <td>{el.user}</td>
@@ -196,10 +224,145 @@ export default function Home(){
                                         })
                                         : 
                                         <td></td>
+                                    } 
+                                </tr>)
+                                }
+                            }) : 
+                            <tr>
+                                <td>Cargando...</td>
+                            </tr>
+                        }
+                       
+                    </tbody>
+                    <tfoot></tfoot>
+                </table>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Jugador</th>
+                            {
+                            dates ? dates.map(el => {
+                                return(
+                                    <th>
+                                        {`${el.day}/${el.month}`}
+                                    </th>
+                                )
+                            }) :
+                            <div>Cargando</div>
+                        }
+                            <th>M1</th>
+                            <th>M2</th>
+                            <th>P</th>
+                            <th>SUM</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <tr className='trA'>
+                            <th>B</th>
+                        </tr>
+                        {
+                            
+                            scoresUser ? scoresUser.map(el => {
+                                var sum = []
+                                if(el.scores[0].categoria === "B"){
+                                return(
+                                <tr>
+                                    <td>{el.user}</td>
+                                    {
+                                    dates ? dates.map(date => {
+                                        return(
+                                            <td>{
+                                                el.scores ? el.scores.filter(elem => 
+                                                elem.month === month
+                                            ).sort((a ,b) => a.day - b.day).map(elem => {
+                                                if(elem.day == date.day){
+                                                    return(
+                                                        elem.totalNeto
+                                                    )
+                                                }
+                                            }) :
+                                            <td>Cargando...</td>}</td>
+                                            )
+                                        }) : 
+                                        <td></td>   
                                     }
                                     
-                                    
+                                    {
+                                        el.scores ? el.scores.sort((a,b) => b.totalNeto - a.totalNeto).slice(-1).map(elem => {
+                                            return(
+                                                <td>{elem.totalNeto}</td>
+                                            )
+                                        })
+                                        : 
+                                        <td></td>
+                                    }
+                                    {
+                                        el.scores ? el.scores.sort((a,b) => b.totalNeto - a.totalNeto).slice(-2).sort((a,b) => a.totalNeto - b.totalNeto).slice(-1).map(elem => {
+                                            if(el.scores.length > 1){
+                                                return(
+                                                    <td>{elem.totalNeto}</td>
+                                                )
+                                            }
+                                            else{
+                                                return(
+                                                    <td></td>
+                                                )
+                                            }
+                                        }) :
+                                        <td></td>
+                                        
+                                    }
+                                    {
+                                        el.scores ? el.scores.sort((a,b) => a.totalNeto - b.totalNeto).slice(-1).map(elem => {
+                                            if(el.scores.length > 2){
+                                                return(
+                                                    <td>{elem.totalNeto}</td>
+                                                )
+                                            }
+                                            else{
+                                                return(
+                                                    <td></td>
+                                                )
+                                            }
+                                        })
+                                        : 
+                                        <td></td>
+                                    }
+                                    {
+                                        el.scores ? el.scores.sort((a,b) => a.totalNeto - b.totalNeto).map(elem => {
+                                            
+                                            if(el.scores.length > 2){
+                                                if(el.scores.indexOf(elem) === 0){
+                                                    sum.push(elem.totalNeto)
+
+                                                    return 
+                                                }
+                                                else if(el.scores.indexOf(elem) === 1){
+                                                    sum.push(elem.totalNeto)
+
+                                                    return 
+                                                }
+                                                else if(el.scores.indexOf(elem) === el.scores.length -1){
+                                                    sum.push(elem.totalNeto)
+
+                                                    return(
+                                                        <td>{sum[0] + sum [1] + sum[2]}</td>
+                                                        
+                                                    )
+                                                }
+                                            }
+                                            else{
+                                                return(
+                                                    <td></td>
+                                                )
+                                            }
+                                        })
+                                        : 
+                                        <td></td>
+                                    } 
                                 </tr>)
+                                }
                             }) : 
                             <tr>
                                 <td>Cargando...</td>
