31f2adac0a981f5c16c1552e84b6b1c1b3f04ef9
Juan Racca <juanracca_@hotmail.com>
2022-04-27 14:27:53 -0300

    color a LandingPage

--- src/Components/LandignPage.jsx
+++ src/Components/LandignPage.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import '../Styles/LandingPage.modules.css';
+import Login from "./Login";
 
 export default function LandingPage(){
     
     return(
-        <div>
-            <div>
-                <h1>Bienvenidos al SDGA Tour!</h1>
-                <img src='LOGO SDGA.jpeg'/>
-                <Link to='/register'>
-                    <button>Registrarse</button>
-                </Link>
-                <Link to='/login'>
-                    <button>Ingresar</button>
-                </Link>
-            </div>
+        <div className='divLanding'>
+            <h1 className='h1landing'>Bienvenidos al SDGA Tour!</h1>
+            {/* <img src='LOGO SDGA.jpeg'/> */}
+            {/* <Link to='/register'>
+                <button>Registrarse</button>
+            </Link> */}
+            <Login></Login>
+            {/* <Link to='/login'>
+                <button>Ingresar</button>
+            </Link> */}
         </div>
     );
 };
