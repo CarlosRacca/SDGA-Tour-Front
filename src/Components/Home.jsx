f1f87fef2696a9ec211947529fc9f11fafdef7e3
Carlos Racca <carlosracca1@gmail.com>
2022-05-09 12:54:11 -0300

    se agrega un espacio abajo de la tabla de la b

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -448,6 +448,7 @@ export default function Home(){
                     </tbody>
                     <tfoot></tfoot>
                 </table>
+                <br />
             </div>
         </main>
     );
