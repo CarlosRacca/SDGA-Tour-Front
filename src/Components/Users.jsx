aae02cff2d7f0dec7d0c87cd76c18dc179e422da
Juan Racca <juanracca_@hotmail.com>
2022-05-10 15:10:09 -0300

    estilos en /users

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -3,34 +3,36 @@ import {useDispatch, useSelector} from 'react-redux';
 import '../Styles/Home.modules.css'
 import { Link } from 'react-router-dom';
 import { getScoresUser } from "../Actions/index";
+import '../Styles/Users.modules.css';
 
 export default function Users(){
 
     const dispatch = useDispatch();
-    const scores = useSelector(state => state.scoresUser)
+    const scores = useSelector(state => state.scoresUser);
 
     useEffect(() => {
-        dispatch(getScoresUser())
-    }, [dispatch])
+        dispatch(getScoresUser());
+    }, [dispatch]);
 
     return(
-        <div>
+        <div className='divU'>
+            <h1 className='title'>JUGADORES</h1>
             <div>
+                <Link to='/home'>
+                    <button className='btnU'>ATRAS</button>
+                </Link>
+            </div>
+            <div className='users'>
                 {scores ? scores.map(el => {
                     return(
-                        <Link to={`/users/${el.id}`}>
-                            <div key={el.id}>{el.user}</div>
-                        </Link>
+                        <a href={`https://sdga-tour.com/users/${el.id}`} >
+                            <div key={el.id} className='player'>{el.user}</div>
+                        </a>
                         )
                     }) : 
                     <div></div>
                 }
             </div>
-            <div>
-                <Link to='/home'>
-                    <button>Volver</button>
-                </Link>
-            </div>
         </div>
-    )
-}
+    );
+};
